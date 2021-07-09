c326b7cf6a0191f30379c1cd8e95c74cd1b27400
darkgraycat <jburyanov@gmail.com>
2021-07-09 10:11:27 +0300

    beautify router route

--- src/router.ts
+++ src/router.ts
@@ -3,9 +3,10 @@ import * as controllers from './controllers'
 
 const router: Router = express.Router()
 
-router.get('*', controllers.read)
-router.post('*', controllers.write)
-router.patch('*', controllers.update)
-router.delete('*', controllers.remove)
+router.route('*')
+  .get(controllers.read)
+  .post(controllers.write)
+  .patch(controllers.update)
+  .delete(controllers.remove)
 
 export default router
