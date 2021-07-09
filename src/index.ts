89a15a316b04d9b16049f13a756164e7f880e1af
darkgraycat <jburyanov@gmail.com>
2021-07-09 12:32:14 +0300

    add PORT constant from process.env

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import express, { Express } from 'express'
 import router from './router'
 
+const PORT = process.env.PORT || 3000
+
 const app: Express = express()
 app.use(router)
 
-app.listen(3000)
+app.listen(PORT, () => {
+  console.log(`Server started at port ${PORT}`)
+})
