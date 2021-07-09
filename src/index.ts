537949c00e8c12e8de377402b2c3c43437213468
darkgraycat <jburyanov@gmail.com>
2021-07-09 12:33:09 +0300

    add type to PORT

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, { Express } from 'express'
 import router from './router'
 
-const PORT = process.env.PORT || 3000
+const PORT: string = process.env.PORT || '3000'
 
 const app: Express = express()
 app.use(router)
